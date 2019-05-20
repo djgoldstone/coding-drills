5183d0df68adc2bfd7f6e3836ea472a80541206f
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-20 15:26:38 -0700

    Formatted variable assignment containing confirm method determining whether user wants boba.  Created conditional if-else statement that logs appropriate response to the console with drinkFlavor concatenated.

--- JAVASCRIPT-Code-Drills/code-drills/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
+++ JAVASCRIPT-Code-Drills/code-drills/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
@@ -5,8 +5,17 @@
 // Using the `confirm` method, welcome the user to the Boba Shop and ask them if they would like a boba
 // Store the user response in a variable `wantsBoba`
 
+var wantsBoba = confirm("Do you want boba?");
+
 
 // Create an if statement checking whether the user wanted boba from the earlier `confirm` dialogue
+
+if (wantsBoba) {
+    var drinkFlavor = prompt("What flavor would you like?");
+    document.write("Your " + drinkFlavor + " boba will be ready soon!");
+} else {
+    document.write("Goodbye! Come back soon!");
+}
 
 
 
