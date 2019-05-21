540821b6880c7de064555607de4d8444ab3a66a0
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-20 17:40:31 -0700

    Iterated through fruits array and counted number of occurances of peach and apple by assigning counter variable to 0, iterating through array, and adding value of one for each instance of the particular word at fruits[i].

--- JAVASCRIPT-Code-Drills/code-drills/00-required-code-drills/07-core-js-arrays-2/challenge-prompt.js
+++ JAVASCRIPT-Code-Drills/code-drills/00-required-code-drills/07-core-js-arrays-2/challenge-prompt.js
@@ -1,26 +1,37 @@
 // declare a variable named ourArray and assign an empty array to it
 
+var ourArray = [];
 
 // we can `push` values into an array with the push method. Documentation can be found here:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
 
 // Let's push the number 1 into ourArray
 
+ourArray.push(1);
 
 // push the string "hello" into ourArray
+
+ourArray.push("hello");
 
 
 // push the boolean false into ourArray
 
+ourArray.push(false);
+
 
 // push the number 84 into ourArray
+
+ourArray.push(84);
 
 
 // push the string "world" into our Array
 
+ourArray.push("world");
+
 
 // console log ourArray
 
+console.log(ourArray);
 
 
 // This seperator is here to make your console logs a little easier to read 
@@ -29,13 +40,15 @@ console.log("--------------------------Seperator 1--------------------------");
 
 // declare a variable named oneToTen and assign it an array containing the numbers from 1 to 10
 
+var oneToTen = [1,2,3,4,5,6,7,8,9,10];
 
 // console log the element at index 4
 
+console.log(oneToTen[4]);
 
 // console log the element at index 7
 
-
+console.log(oneToTen[7]);
 
 
 // This seperator is here to make your console logs a little easier to read 
@@ -46,24 +59,43 @@ console.log("--------------------------Seperator 2--------------------------");
 var numArray = [2, 65, 3, 7, 39, 22, 11, 94, 299, 9, 20, 21, 51, 37];
 // iterate through numArray and create an individual console log for every number greater than 50
 
-
+for (var i = 0; i < numArray.length; i++) {
+  if (numArray[i] > 50) {
+    console.log(numArray[i]);
+  }
+}
 
 // iterate through numArray and create an alert that tells us 
 // the index of all the occurances of the number 11
 
-
-
+for (var i = 0; i < numArray.length; i++) {
+  if (numArray[i] === 11) {
+    alert(numArray.indexOf(numArray[i]));
+  }
+}
 // iterate through numArray and console log the sum of all the numbers
 
-
+var sum = 0;
+for (var i = 0; i < numArray.length; i++) {
+  sum += numArray[i];
+}
+console.log(sum);
 
 // iterate through numArray and console log the sum of all the numbers greater than 50
 
-
+for (var i=0; i < numArray.length; i++) {
+  if (numArray[i] > 50) {
+    console.log(numArray[i]);
+  }
+}
 
 // iterate through numArray and console log the sum of all the even numbers
 
-
+for (var i=0; i < numArray.length; i++) {
+  if (numArray[i] % 2 === 0) {
+    console.log(numArray[i]);
+  }
+}
 
 // This seperator is here to make your console logs a little easier to read 
 // by creating seperations between the different sections of the activity
@@ -75,9 +107,23 @@ var fruits = [
   "Kiwi", "Apple", "Watermelon", "Lemon", "Pomelo", "Apple", "Banana", "Peach", "Apricot", "Grape"];
 // iterate through fruits and console log the number of times "Apple" appears in the array
 
-
+var counter = 0;
+for (var i = 0; i < fruits.length; i++) {
+  if (fruits[i] === "Apple") {
+    counter += 1;
+  }
+}
+console.log(counter);
 
 // iterate through fruits and console log the number of times "Peach" appears in the array
+
+var counter = 0;
+for (var i = 0; i < fruits.length; i++) {
+  if (fruits[i] === "Peach") {
+    counter += 1;
+  }
+}
+console.log(counter);
 
 
 
