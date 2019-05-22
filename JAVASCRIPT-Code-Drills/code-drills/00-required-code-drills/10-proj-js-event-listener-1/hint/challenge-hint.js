53bb4e95c01595c25edda6cf75f410b5a4ea791b
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-21 19:17:41 -0700

    Wrote conditional statement checking if userKey evaluated to k after first key event.  If k is pressed and the user dialogue evaluates to true an alert will be issued saying Hello, else another alert will be issued.

--- JAVASCRIPT-Code-Drills/code-drills/00-required-code-drills/10-proj-js-event-listener-1/hint/challenge-hint.js
+++ JAVASCRIPT-Code-Drills/code-drills/00-required-code-drills/10-proj-js-event-listener-1/hint/challenge-hint.js
@@ -4,19 +4,31 @@ var dialogue;
 
 // create a key press listener
 document.onkeyup = function(input) {
-  dialogue = input.key.toLowerCase();
+  var userKey = input.key.toLowerCase();
 // store the key the user pressed into a variable so we can 
   // refer to it easier in this function and
   // change it to lower case to make our logic not case sensitive
 
-  console.log(dialogue);
+  console.log(userKey);
   // console log the key that the user pressed
 
-  
+  if (userKey === "h") {
+    dialogue = confirm("You got it!");
+  }
+
+  if (userKey === "k") {
+    if (dialogue === true) {
+      alert("Hello");
+    } else {
+      alert("No thanks");
+    }
+  }
   // check whether the user pressed the "h" key
 
     // create a confirm dialogue when they do and store the response from that
     // dialogue into the variable we declared outside the event listener
+
+    
 
 
 
