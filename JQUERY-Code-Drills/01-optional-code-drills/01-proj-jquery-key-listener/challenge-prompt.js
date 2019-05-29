a787433693357930c236e4d64db06da33899bc37
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-28 22:45:15 -0700

    Declared variables containing the elements at id's letterTyped and lastLetter. Created an empty array for used letters.

--- JQUERY-Code-Drills/01-optional-code-drills/01-proj-jquery-key-listener/challenge-prompt.js
+++ JQUERY-Code-Drills/01-optional-code-drills/01-proj-jquery-key-listener/challenge-prompt.js
@@ -1,11 +1,14 @@
 // Section 1 
 // Set a variable of userTyped and set it to point at the element in the DOM with an id of "letterTyped"
+var userTyped = document.getElementById("letterTyped");
 
 
 // Set a variable of letterUsed and set it to point at the element in the DOM with an id of "lastLetter"
+var letterUsed = document.getElementById("lastLetter");
 
 
 // create a variable called LetterUsedArray and have it equal and empty array
+var LetterUsedArray = [];
 
 
 
@@ -13,9 +16,11 @@
 // now use the onkey up jquery function and pass it the argument of event
 document.onkeyup = function (event) {
     // now set a variable of userInput equal to the event.key
+    userInput = event.key;
 
     
     // use the userTyped variable and the textContent property equal the userInput
+    
 
     
     // push the userInput into the letterUsedArray
