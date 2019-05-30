0254df4d94e710b38bb0fdc0ea78d085866a07d0
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-29 22:54:24 -0700

    Initial commit of Timers code drills.

--- JQUERY-Code-Drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
+++ JQUERY-Code-Drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
@@ -10,76 +10,28 @@ $(document).ready(function () {
     });
     $(document).on("click", ".letterC", function() {
         $("#text-div").append("C");
-    });
+    })
     $(document).on("click", ".letterD", function() {
         $("#text-div").append("D");
-    });
-    $(document).on("click", ".letterE", function() {
+    })
+    $(document).on("click", ".letterE", function(){
         $("#text-div").append("E");
-    });
-    $(document).on("click", ".letterF", function() {
+    })
+    $(document).on("click", "letterF", function() {
         $("#text-div").append("F");
-    });
-    $(document).on("click", ".letterG", function() {
+    })
+    $(document).on("click", "letterG", function() {
         $("#text-div").append("G");
-    });
-    $(document).on("click", ".letterH", function() {
-        $("text-div").append("H");
-    });
-    $(document).on("click", ".letterI", function() {
+    })
+    $(document).on("click", "letterH", function() {
+        $("#text-div").append("H");
+    })
+    $(document).on("click", "letterI", function() {
         $("#text-div").append("I");
-    });
-    $(document).on("click", ".letterJ", function() {
+    })
+    $(document).on("click", "letterJ", function() {
         $("#text-div").append("J");
-    });
-    $(document).on("click", ".letterK", function() {
-        $("#text-div").append("K");
-    });
-    $(document).on("click", ".letterL", function() {
-        $("#text-div").append("L");
-    });
-    $(document).on("click", ".letterM", function() {
-        $("#text-div").append("M");
-    });
-    $(document).on("click", ".letterN", function() {
-        $("#text-div").append("N");
-    });
-    $(document).on("click", ".letterO", function() {
-        $("#text-div").append("O");
-    });
-    $(document).on("click", ".letterP", function() {
-        $("#text-div").append("P");
-    });
-    $(document).on("click", ".letterQ", function() {
-        $("#text-div").append("Q");
-    });
-    $(document).on("click", ".letterR", function() {
-        $("#text-div").append("R");
-    });
-    $(document).on("click", ".letterS", function() {
-        $("#text-div").append("S");
-    });
-    $(document).on("click", ".letterT", function() {
-        $("#text-div").append("T");
-    });
-    $(document).on("click", ".letterU", function() {
-        $("#text-div").append("U");
-    });
-    $(document).on("click", ".letterV", function() {
-        $("#text-div").append("V");
-    });
-    $(document).on("click", ".letterW", function() {
-        $("#text-div").append("W");
-    });
-    $(document).on("click", ".letterX", function() {
-        $("#text-div").append("X");
-    });
-    $(document).on("click", ".letterY", function() {
-        $("#text-div").append("Y");
-    });
-    $(document).on("click", ".letterZ", function() {
-        $("#text-div").append("Z");
-    });
+    })
 
     // Write a series of on click listeners for each letter in the html
     // Whenever the user clicks on a letter, you want to append the corresponding letter to the `text-div` element
