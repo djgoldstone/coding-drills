cd3b713fa92c669c4ed71537f637a4022f79f042
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-04 09:02:43 -0700

    Prevented default behavior from submitting. Stored the value of showSearch in a variable. Added variable to the end of api query url. Appended images, show name, genres, and summary to the DOM.

--- AJAX-Drills/00-required-code-drills/03-core-tv-api/challenge-prompt.js
+++ AJAX-Drills/00-required-code-drills/03-core-tv-api/challenge-prompt.js
@@ -1,23 +1,34 @@
 $(document).submit(function () {
     // prevent the default behavior of the submit
-
+    event.preventDefault();
     // grab the show information from the form on the page
     var searchInformation;
-
+    searchInformation = $("#showSearch").val();
 
     // console.log the input
-
+    console.log(searchInformation);
 
     // this is the api we are using add your search variable to end of the queryURL
-    var queryURL = "http://api.tvmaze.com/search/shows?q=";
+    var queryURL = "http://api.tvmaze.com/search/shows?q=" + searchInformation;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         // console.log the response 
+        console.log(response);
 
         // create a forloop and append the first 3 responses to the page
+        for (var i = 0; i < 3; i++) {
+            console.log(response[0].show.image.medium);
+            $(".shows").append("<img src=" + response[0].show.image.medium + ">");
+            console.log(response[i].show.name);
+            $(".shows").append(response[i].show.name);
+            console.log(response[i].show.genres);
+            $(".shows").append(response[i].show.genres);
+            console.log(response[i].show.summary);
+            $(".shows").append(response[i].show.summary);
+        }
 
             // console.log the image and then append it to the page.            
 
