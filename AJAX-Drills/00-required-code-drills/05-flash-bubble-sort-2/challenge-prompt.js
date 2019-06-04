996755c696412d23cf2f181f6d2faebaeb53e3ce
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-04 15:22:23 -0700

    Created queryURL, logged response and author at index 0 to console. Then iterated through response.articles with a condition that if author did not have a value it would be replaced with my name. Else, the JQuery append method would access the appendMe div class in order to display title, author, and description at their respective keys with HTML tags added for styling.

--- AJAX-Drills/00-required-code-drills/05-flash-bubble-sort-2/challenge-prompt.js
+++ AJAX-Drills/00-required-code-drills/05-flash-bubble-sort-2/challenge-prompt.js
@@ -9,7 +9,20 @@
 
 function bubbleSortTwo(arr) {
     // ================= code goes here ===========================
-
+var swap = true;
+while (swap) {
+    swap = false;
+    for (var i = 0; i < arr.length -1; i++) {
+        if (arr[i] > arr[i+1]) {
+            var placeHolder = arr[i];
+            arr[i] = arr[i+1];
+            arr[i + 1] = placeHolder;
+            swap = true;
+        }
+    }
+    
+}
+return arr;
     
 
 
